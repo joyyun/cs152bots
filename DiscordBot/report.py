5f23afb4116ba973bb9fe43572b3149d613ca898
Peng Hao Lu <pengluh@gmail.com>
2024-05-09 15:30:08 -0700

    flow template

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -2,11 +2,14 @@ from enum import Enum, auto
 import discord
 import re
 
+
 class State(Enum):
     REPORT_START = auto()
     AWAITING_MESSAGE = auto()
     MESSAGE_IDENTIFIED = auto()
     REPORT_COMPLETE = auto()
+    AWAITING_CATEGORY = auto()
+
 
 class Report:
     START_KEYWORD = "report"
@@ -17,56 +20,94 @@ class Report:
         self.state = State.REPORT_START
         self.client = client
         self.message = None
-    
+
     async def handle_message(self, message):
-        '''
-        This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what 
+        """
+        This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what
         prompts to offer at each of those states. You're welcome to change anything you want; this skeleton is just here to
-        get you started and give you a model for working with Discord. 
-        '''
+        get you started and give you a model for working with Discord.
+        """
 
         if message.content == self.CANCEL_KEYWORD:
             self.state = State.REPORT_COMPLETE
             return ["Report cancelled."]
-        
+
         if self.state == State.REPORT_START:
-            reply =  "Thank you for starting the reporting process. "
+            reply = "Thank you for starting the reporting process. "
             reply += "Say `help` at any time for more information.\n\n"
             reply += "Please copy paste the link to the message you want to report.\n"
             reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
             self.state = State.AWAITING_MESSAGE
             return [reply]
-        
+
         if self.state == State.AWAITING_MESSAGE:
             # Parse out the three ID strings from the message link
-            m = re.search('/(\d+)/(\d+)/(\d+)', message.content)
+            m = re.search("/(\d+)/(\d+)/(\d+)", message.content)
             if not m:
-                return ["I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel."]
+                return [
+                    "I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel."
+                ]
             guild = self.client.get_guild(int(m.group(1)))
             if not guild:
-                return ["I cannot accept reports of messages from guilds that I'm not in. Please have the guild owner add me to the guild and try again."]
+                return [
+                    "I cannot accept reports of messages from guilds that I'm not in. Please have the guild owner add me to the guild and try again."
+                ]
             channel = guild.get_channel(int(m.group(2)))
             if not channel:
-                return ["It seems this channel was deleted or never existed. Please try again or say `cancel` to cancel."]
+                return [
+                    "It seems this channel was deleted or never existed. Please try again or say `cancel` to cancel."
+                ]
             try:
                 message = await channel.fetch_message(int(m.group(3)))
             except discord.errors.NotFound:
-                return ["It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."]
+                return [
+                    "It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."
+                ]
 
             # Here we've found the message - it's up to you to decide what to do next!
             self.state = State.MESSAGE_IDENTIFIED
-            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
-        
+            return [
+                "I found this message:",
+                "```" + message.author.name + ": " + message.content + "```",
+                "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!",
+            ]
+
         if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+            # move onto stage 2 of reporting flow
+            self.state = State.AWAITING_CATEGORY
+            return [
+                "What category would you like to report this message under? Please respond with 'option1', 'option2', etc."
+            ]
+
+        if self.state == State.AWAITING_CATEGORY:
+            # move onto stage 3 of reporting flow
+            # parse our category from response
+            # TODO: update regex
+            category = re.search("/(\d+)/(\d+)/(\d+)", message.content)
+            if not category:
+                return [
+                    "I'm sorry, I couldn't understand the category. Please respond with 'option1', 'option2', etc."
+                ]
+
+            if category == "option1":
+                self.state = State.REPORT_COMPLETE
+                return [
+                    "You've selected option1. Please provide any additional details you'd like to include in your report."
+                ]
+            elif category == "option2":
+                self.state = State.REPORT_COMPLETE
+                return [
+                    "You've selected option2. Please provide any additional details you'd like to include in your report."
+                ]
+            else:
+                return [
+                    "I'm sorry, I couldn't understand the category. Please respond with 'option1', 'option2', etc."
+                ]
+
+        if self.state == State.REPORT_COMPLETE:
+            return ["Thank you for your report. It has been submitted."]
 
         return []
 
     def report_complete(self):
         return self.state == State.REPORT_COMPLETE
-    
-
-
-    
-
