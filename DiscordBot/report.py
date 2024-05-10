2385972eda232d2f0f6efcdd0bb924b4ccb8a951
joyyun <joyyun2012@gmail.com>
2024-05-09 18:06:28 -0700

    completed user chat flow

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -9,6 +9,10 @@ class State(Enum):
     MESSAGE_IDENTIFIED = auto()
     REPORT_COMPLETE = auto()
     AWAITING_CATEGORY = auto()
+    IMMINENT_DANGER_SELECTION = auto()
+    IMMINENT_DANGER = auto()
+    ADDITIONAL_MESSAGE = auto()
+    AWAITING_ADDITIONAL_MESSAGE = auto()
     FINAL = auto()
 
 
@@ -16,7 +20,7 @@ class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
-    CATEGORIES = "'option1', 'option2', or 'option3'"
+    CATEGORIES = "'spam', 'inappropriate content', 'hate speech', 'imminent danger', or 'other'"
 
     def __init__(self, client):
         self.state = State.REPORT_START
@@ -24,6 +28,7 @@ class Report:
         self.message = None
 
     async def handle_message(self, message):
+        print(self.state)
         """
         This function makes up the meat of the user-side reporting flow. It defines how we transition between states and what
         prompts to offer at each of those states. You're welcome to change anything you want; this skeleton is just here to
@@ -47,7 +52,7 @@ class Report:
             m = re.search("/(\d+)/(\d+)/(\d+)", message.content)
             if not m:
                 return [
-                    "I'm sorry, I couldn't read that link. Please try again or say `cancel` to cancel."
+                    "I'm sorry, I could not read the message link you sent. Please try again or say `cancel` to cancel."
                 ]
             guild = self.client.get_guild(int(m.group(1)))
             if not guild:
@@ -92,27 +97,80 @@ class Report:
 
         if self.state == State.AWAITING_CATEGORY:
             category = message.content
-            if not category:
-                return [
-                    "I'm sorry, I couldn't understand the category. Please respond with "
-                    + self.CATEGORIES
-                    + "."
+            if category in ["spam", "inappropriate content", "hate speech"]:
+                self.state = State.ADDITIONAL_MESSAGE
+                return [ # TODO: Insert description of what this abuse type is + our policy on it in text below
+                    f"You've selected that this message falls under: {category}. Please provide any additional details you'd like to include in your report."
                 ]
-
-            if category == "option1":
-                self.state = State.FINAL
+            elif category == "other":
+                self.state = State.ADDITIONAL_MESSAGE
                 return [
-                    "You've selected option1. Please provide any additional details you'd like to include in your report."
+                    f"You've selected: {category}. Please explain why you reported this message and provide any additional details you'd like to include in your report."
                 ]
-            elif category == "option2":
-                self.state = State.FINAL
+            elif category == "imminent danger":
+                self.state = State.IMMINENT_DANGER_SELECTION
                 return [
-                    "You've selected option2. Please provide any additional details you'd like to include in your report."
+                    f"Thank you for your urgency. You've selected: {category}. To further inform the action we should take, please select which category this falls under.
+                        Respond with 'sh' for self-harm or suicidal intent, 'ct' for credible threat of violence, or 'k' for kidnapping threat." 
+                        # TODO: maybe we should add an 'other' category here as well?
                 ]
             else:
                 return [
-                    "I'm sorry, I couldn't understand the category. Please respond with 'option1', 'option2', etc."
+                    "I'm sorry, I couldn't understand the category. Please respond with " + self.CATEGORIES + "."
                 ]
+            
+        if self.state == State.IMMINENT_DANGER_SELECTION:
+            category = message.content
+            if category in ['sh', 'ct']:
+                self.state = State.ADDITIONAL_MESSAGE
+                if category == 'sh':
+                    type = "self-harm or suicidal intent"
+                elif category == 'ct':
+                    type = "credible threat of violence"
+                return [ # TODO: Insert description of what this abuse type is + our policy on it in text below
+                    f"You've selected that this message falls under: {type}. Our moderator's have been notified of this report. Please provide
+                        any additional details you'd like to include in your report."
+                ]
+            elif category == "k":
+                self.state = State.ADDITIONAL_MESSAGE
+                return [
+                    f"You've selected that this message falls under: kidnapping threat. Our moderator's have been notified of this report, and the
+                        message is being run through our AI-detection model. Please provide any additional details you'd like to include in your report."
+                ]
+            elif category == "imminent danger":
+                self.state = State.IMMINENT_DANGER_SELECTION
+                return [
+                    f"Thank you for your urgency. You've selected: {category}. Our moderator's have been notified of this report, and the message is being run through
+                        our AI-detection model. Please provide any additional details you'd like to include in your report."
+                ]
+            else:
+                return [
+                    f"I'm sorry, I couldn't understand the category. Respond with 'sh' for self-harm or suicidal intent, 'ct' for credible threat of 
+                        violence, or 'k' for kidnapping."
+                ]
+            
+        if self.state == State.ADDITIONAL_MESSAGE:
+            self.state = State.AWAITING_ADDITIONAL_MESSAGE
+            return [
+                "Are there other messages you would like to flag? Please respond with 'yes' or 'no'. By saying 'no', your report will be submitted.",
+            ]
+        
+        if self.state == State.AWAITING_ADDITIONAL_MESSAGE:
+            if message.content.lower() == "yes":
+                reply =  "The same process will be repeated for the next message.\n\n" 
+                reply += "Thank you for starting the reporting process. "
+                reply += "Say `help` at any time for more information.\n\n"
+                reply += "Please copy paste the link to the message you want to report.\n"
+                reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
+                self.state = State.AWAITING_MESSAGE
+                return [reply]
+            elif message.content.lower() == "no":
+                self.state = State.FINAL
+            else:
+                return [
+                    "I'm sorry, I didn't understand that. Please respond with 'yes' or 'no'. By saying 'no', your report will be submitted."
+                ]
+        
 
         if self.state == State.FINAL:
             # TODO: fix
