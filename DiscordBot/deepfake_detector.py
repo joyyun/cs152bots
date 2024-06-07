a6c4fcd39b41d27c3420c5100eb18e49f74733d4
Peng Hao Lu <pengluh@gmail.com>
2024-06-06 23:07:39 -0700

    autoformat

--- DiscordBot/deepfake_detector.py
+++ DiscordBot/deepfake_detector.py
@@ -11,13 +11,15 @@ feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
 
 # Testing URL
 # URL = 'https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg'
-    
+
+
 # Preprocess the image
 def preprocess_image(url):
     response = requests.get(url)
     image = Image.open(BytesIO(response.content)).convert("RGB")
     inputs = feature_extractor(images=image, return_tensors="pt")
     return inputs
+
 
 def predict_deepfake(url):
     inputs = preprocess_image(url)
@@ -32,9 +34,9 @@ def predict_deepfake(url):
         ret += "The image is not AI-generated (real). \n"
     return ret
 
+
 def predict_deepfake_nopreprocessing(url):
-    pipe = pipeline('image-classification', model=model_name, device=-1)
+    pipe = pipeline("image-classification", model=model_name, device=-1)
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
     return pipe(img)
-
